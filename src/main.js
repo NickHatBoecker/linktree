5b7a7ffd2786b9f88ccc7f091432c0b6459667e2
NickHatBoecker <info@nick-hat-boecker.de>
2021-03-10 00:32:06 +0100

    Add github icon

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
-import { faTwitter, faYoutube, faItunesNote } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faYoutube, faItunesNote, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import * as Contentful from 'contentful'
 
-library.add([faGlobe, faTwitter, faYoutube, faItunesNote])
+library.add([faGlobe, faTwitter, faYoutube, faItunesNote, faGithub])
 
 Vue.component('fa-icon', FontAwesomeIcon)
 
